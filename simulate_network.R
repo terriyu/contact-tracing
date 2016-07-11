3534613c5f76e03fb2274afda8a31fed5ebed8fd
Terri Yu <terri@terriyu.info>
2016-07-11 16:32:24 -0400

    Move error checking, rename variables in simulate_network.R
    * Add leading dot to helper functions
    * Remove nonstochastic variable (not needed)
    * Rename initial.infect initial.infect.method for clarity
    * Rename Z0 and W, Z0.fixed and W.fixed for clarity
    * Revised tests accordingly
    * Add a few more tests for simulate_network.R

--- simulate_network.R
+++ simulate_network.R
@@ -11,9 +11,11 @@ suppressMessages(library(network))
 
 # ---------------- FUNCTIONS ---------------------
 
-create.edge.prob.mtx <- function(nodes.per.class, P.ij) {
+.create.edge.prob.mtx <- function(nodes.per.class, P.ij) {
   # Creates a matrix whose entries are probabilities that an edge will link
   # each pair of nodes
+  #
+  # NOTE: This is a private helper function for generate.network()
   #
   # Args:
   #   nodes.per.class - vector containing the number of nodes in class 1,
@@ -31,27 +33,6 @@ create.edge.prob.mtx <- function(nodes.per.class, P.ij) {
   num.classes <- length(nodes.per.class)
   # Number of nodes in network
   num.nodes <- sum(nodes.per.class)
-
-  # Error checking: check that P.ij is square matrix
-  if (length(P.ij) > 1) {
-    if (dim(P.ij)[1] != dim(P.ij)[2]) {
-      stop("P.ij not a square matrix")
-    }
-  }
-
-  # Error checking: check that P.ij is symmetric matrix
-  # Probability of link between class i and class j should be same as
-  # probability of link between class j and class i
-  if (! all(P.ij == t(P.ij))) {
-    stop("P.ij is not symmetric")
-  }
-
-  # Error checking: check that nodes.per.class and P.ij are consistent
-  if (length(P.ij) > 1) {
-    if (num.classes != dim(P.ij)[1]) {
-      stop("nodes.per.class and P.ij have inconsistent dimensions")
-    }
-  }
 
   # Case where there is only one node class
   if ((num.classes == 1) && (length(P.ij) == 1)) {
@@ -88,10 +69,33 @@ generate.network <- function(nodes.per.class, P.ij) {
   # Returns:
   #   socio.net - network object containing the simulated network
 
+  # Error checking: check that P.ij is square matrix
+  if (length(P.ij) > 1) {
+    if (dim(P.ij)[1] != dim(P.ij)[2]) {
+      stop("P.ij not a square matrix")
+    }
+  }
+
+  # Error checking: check that P.ij is symmetric matrix
+  # Probability of link between class i and class j should be same as
+  # probability of link between class j and class i
+  if (! all(P.ij == t(P.ij))) {
+    stop("P.ij is not symmetric")
+  }
+
+  # Error checking: check that nodes.per.class and P.ij are consistent
+  if (length(P.ij) > 1) {
+    # Number of classes
+    num.classes <- length(nodes.per.class)
+    if (num.classes != dim(P.ij)[1]) {
+      stop("nodes.per.class and P.ij have inconsistent dimensions")
+    }
+  }
+
   # Number of nodes in network
   num.nodes <- sum(nodes.per.class)
   # Create matrix containing prob each node is linked to another node
-  prob.mtx <- create.edge.prob.mtx(nodes.per.class, P.ij)
+  prob.mtx <- .create.edge.prob.mtx(nodes.per.class, P.ij)
   # Set lower triangle to zero, so we only generate a link between i and j once
   prob.mtx[lower.tri(prob.mtx)] <- 0
 
@@ -110,19 +114,18 @@ generate.network <- function(nodes.per.class, P.ij) {
   return(socio.net)
 }
 
-spread.infection <- function(socio.net, eta, tau, initial.infect = "nodes", num.infect = NULL, verbose = FALSE, nonstochastic = FALSE, Z0 = NULL, W = NULL) {
+spread.infection <- function(socio.net, eta, tau, initial.infect.method, num.infect = NULL, verbose = FALSE, Z0.fixed = NULL, W.fixed = NULL) {
   # Spread infection across given network and return infected network
   #
   # Args:
   #   socio.net - uninfected, undirected network (needs to be network object)
   #   eta - Bernoulli process parameter for initial infection
   #   tau - Bernoulli process parameter for generating transmission matrix
-  #   initial.infect - string specifying method to use for initial infection "nodes", "nodes_fixed", or "edges"
-  #   num.infect - number of nodes or edges in initial infection (fixed number)
-  #   verbose - flag to print variables during infection process (for debugging)
-  #   nonstochastic - flag to use pre-determined values for Z0 and W (for debugging)
-  #   Z0 - (to be specified if nonstochastic = TRUE) vector of initial infected nodes
-  #   W - (to be specified if nonstochastic = TRUE) infection trasmission matrix
+  #   initial.infect.method - string specifying method to use for initial infection "nodes", "nodes_fixed", or "edges"
+  #   num.infect - [optional] fix number of nodes or edges in initial infection to avoid returning empty infection
+  #   verbose - [optional] flag to print variables during infection process (for debugging)
+  #   Z0.fixed - [optional] vector of initial infected nodes (only use for debugging/testing)
+  #   W.fixed - [optional] infection trasmission matrix (only use for debugging/testing)
   #
   # Returns:
   #   Z0 - vector of initial infected nodes (1 for infected, 0 for not infected)
@@ -133,19 +136,12 @@ spread.infection <- function(socio.net, eta, tau, initial.infect = "nodes", num.
   # Error checking for eta and tau parameters
   if (! is.null(eta)) {
     if ((eta < 0) | (eta > 1)) {
-      stop("Bernoulli process parameter eta needs to be between 0 and 1")
+      stop("Bernoulli process parameter eta must be between 0 and 1")
     }
   }
   if (! is.null(tau)) {
     if ((tau < 0) | (tau > 1)) {
-      stop("Bernoulli process parameter tau needs to be between 0 and 1")
-    }
-  }
-
-  # Error checking: make sure Z0 and W are specified if in nonstochastic mode
-  if (nonstochastic) {
-    if (is.null(Z0) | is.null(W)) {
-      stop("If nonstochastic flag is TRUE, need to specify values for Z0 and W")
+      stop("Bernoulli process parameter tau must be between 0 and 1")
     }
   }
 
@@ -160,18 +156,18 @@ spread.infection <- function(socio.net, eta, tau, initial.infect = "nodes", num.
   # Multiple ways to do this: see tosource.R
   # TODO: Implement other ways of generating initial infection [see tosource.r spread()]
   #       In particular, infecting a fixed number of edges is not implemented
-  if (! nonstochastic) {
-    if (initial.infect == "nodes") {
+  if (is.null(Z0.fixed)) {
+    if (initial.infect.method == "nodes") {
       # Independent Bernoulli infection process on all nodes
       Z0 <- rbinom(num.nodes, 1, eta)
-    } else if ((initial.infect == "nodes_fixed") & (! is.null(num.infect))) {
-      if ((num.infect < 0) | (num.infect > num.nodes)) {
-        stop("num.infect must be between 0 and the number of nodes in the network")
+    } else if ((initial.infect.method == "nodes_fixed") & (! is.null(num.infect))) {
+      if ((num.infect < 1) | (num.infect > num.nodes)) {
+        stop("num.infect must be between 1 and the number of nodes in the network")
       }
       # Randomly pick num.infect nodes to infect
       Z0 <- rep(0, num.nodes)
       Z0[sample(1:num.nodes, num.infect)] <- 1
-    } else if (initial.infect == "edges") {
+    } else if (initial.infect.method == "edges") {
       # Independent Bernoulli infection process on all edges
       # Pick edges to infect
       edge.infect <- rbinom(num.edges, 1, eta)
@@ -185,15 +181,20 @@ spread.infection <- function(socio.net, eta, tau, initial.infect = "nodes", num.
       Z0 <- rep(0, num.nodes)
       Z0[nodes.infect.idx] <- 1
     } else {
-      stop("initial.infect incorrectly specified")
+      stop("initial.infect.method incorrectly specified")
     }
+  } else {
+    Z0 <- Z0.fixed
   }
 
   # Transmissibility matrix W (1 for edge that can transmit infection, 0 otherwise)
   # Assume independent homogenous Bernoulli processes on all nodes wih parameter tau
-  if (! nonstochastic) {
+  if (is.null(W.fixed)) {
     W <- matrix(rbinom(num.nodes^2, 1, tau), nrow = num.nodes, ncol = num.nodes)
+  } else {
+    W <- W.fixed
   }
+
   diag(W) <- 0 # A node can't transmit infection to itself
   # Multiply element-wise by sociomatrix to get all possible transmissions in
   # this particular network instance
--- tests/testthat/test_simulate_network.R
+++ tests/testthat/test_simulate_network.R
@@ -4,15 +4,8 @@ source('../../simulate_network.R')
 
 test_that("Test create.edge.prob.mtx", {
 
-  # Test non-square P.ij matrix
-  expect_error(create.edge.prob.mtx(c(25,5,25), matrix(runif(10), nrow=2, ncol=5)), regexp = "P.ij not a square matrix")
-  # Test non-symmetric P.ij matrix
-  expect_error(create.edge.prob.mtx(c(25,5,25), matrix(1:9, nrow=3, ncol=3)), regexp = "P.ij is not symmetric")
-  # Test nodes.per.class and P.ij are consistent
-  expect_error(create.edge.prob.mtx(c(25,5,25), matrix(rep(1,4), nrow=2, ncol=2)), regexp = "nodes.per.class and P.ij have inconsistent dimensions")
-
-  # Test for case where there is only one node class
-  expect_equal(create.edge.prob.mtx(3, 0.5), matrix(c(0, 0.5, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 0), nrow = 3, ncol = 3))
+ # Test for case where there is only one node class
+  expect_equal(.create.edge.prob.mtx(3, 0.5), matrix(c(0, 0.5, 0.5, 0.5, 0, 0.5, 0.5, 0.5, 0), nrow = 3, ncol = 3))
 
   # Test for case where there is more than one node class
   # Number of nodes in each class
@@ -21,15 +14,24 @@ test_that("Test create.edge.prob.mtx", {
   P.ij <- matrix(c(0.5, 0.4, 0, 0.4, 0.8, 0.4, 0, 0.4, 0.5), nrow = 3, ncol = 3)
   # Expected matrix
   edge.prob.mtx <- matrix(c(0, 0.4, 0.4, 0, 0, 0, 0.4, 0, 0.8, 0.4, 0.4, 0.4, 0.4, 0.8, 0, 0.4, 0.4, 0.4, 0, 0.4, 0.4, 0, 0.5, 0.5, 0, 0.4, 0.4, 0.5, 0, 0.5, 0, 0.4, 0.4, 0.5, 0.5, 0), nrow = 6, ncol = 6)
-  expect_equal(create.edge.prob.mtx(nodes.per.class, P.ij), edge.prob.mtx)
+  expect_equal(.create.edge.prob.mtx(nodes.per.class, P.ij), edge.prob.mtx)
 })
 
 test_that("Test generate.network", {
   set.seed(137)
+
+  # Test non-square P.ij matrix
+  expect_error(generate.network(c(25,5,25), matrix(runif(10), nrow=2, ncol=5)), regexp = "P.ij not a square matrix")
+  # Test non-symmetric P.ij matrix
+  expect_error(generate.network(c(25,5,25), matrix(1:9, nrow=3, ncol=3)), regexp = "P.ij is not symmetric")
+  # Test nodes.per.class and P.ij are consistent
+  expect_error(generate.network(c(25,5,25), matrix(rep(1,4), nrow=2, ncol=2)), regexp = "nodes.per.class and P.ij have inconsistent dimensions")
+
   # Number of nodes in each class
   nodes.per.class <- c(1,2,3)
   # Matrix containing probability of link between class i and class j
   P.ij <- matrix(c(0.5, 0.4, 0, 0.4, 0.8, 0.4, 0, 0.4, 0.5), nrow = 3, ncol = 3)
+
   net <- generate.network(nodes.per.class, P.ij)
   socio.mtx <- as.sociomatrix(net)
 
@@ -42,31 +44,30 @@ test_that("Test generate.network", {
 
 test_that("Test spread.infection", {
   set.seed(137)
+
   net <- network(matrix(c(0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0), nrow = 6, ncol = 6), directed = FALSE)
   W <- matrix(c(0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), nrow = 6, ncol = 6)
   Z0 <- c(1,0,0,0,0,0)
   Z.expect <- c(1, 1, 1, 1, 0, 0) # Expected value for Z after infection has been spread over network
 
   # Test error checking for valid Bernoulli parameter eta
-  expect_error(spread.infection(net, -9, NULL), regexp = "Bernoulli process parameter eta needs to be between 0 and 1")
+  expect_error(spread.infection(net, -9, NULL, "nodes_fixed"), regexp = "Bernoulli process parameter eta must be between 0 and 1")
+  expect_error(spread.infection(net, 81, NULL, "nodes_fixed"), regexp = "Bernoulli process parameter eta must be between 0 and 1")
   # Test error checking for valid Bernoulli parameter tau
-  expect_error(spread.infection(net, NULL, 100), regexp = "Bernoulli process parameter tau needs to be between 0 and 1")
+  expect_error(spread.infection(net, NULL, -2, "edges"), regexp = "Bernoulli process parameter tau must be between 0 and 1")
+  expect_error(spread.infection(net, NULL, 100, "edges"), regexp = "Bernoulli process parameter tau must be between 0 and 1")
 
-  # Test error checking for Z0 and W when nonstochastic flag is set to TRUE
-  expect_error(spread.infection(net, NULL, NULL, "nodes", NULL, FALSE, TRUE), regexp = "If nonstochastic flag is TRUE, need to specify values for Z0 and W")
-  expect_error(spread.infection(net, NULL, NULL, "nodes", NULL, FALSE, TRUE, Z0), regexp = "If nonstochastic flag is TRUE, need to specify values for Z0 and W")
-  expect_error(spread.infection(net, NULL, NULL, "nodes", NULL, FALSE, TRUE, NULL, W), regexp = "If nonstochastic flag is TRUE, need to specify values for Z0 and W")
-
-  # Test error checking for initial.infect variable
-  expect_error(spread.infection(net, 0.2, 0.1, "invalid"), regexp = "initial.infect incorrectly specified")
-  expect_error(spread.infection(net, 0.2, 0.1, "nodes_fixed", 10), regexp = "num.infect must be between 0 and the number of nodes in the network")
+  # Test error checking for initial.infect.method variable
+  expect_error(spread.infection(net, 0.2, 0.1, "invalid"), regexp = "initial.infect.method incorrectly specified")
+  expect_error(spread.infection(net, 0.2, 0.1, "nodes_fixed", 10), regexp = "num.infect must be between 1 and the number of nodes in the network")
+  expect_error(spread.infection(net, 0.2, 0.1, "nodes_fixed", 0), regexp = "num.infect must be between 1 and the number of nodes in the network")
 
   # Test that number of infected nodes in Z0 matches given number, if number of infected nodes is fixed ahead of time
   result <- spread.infection(net, NULL, 0.1, "nodes_fixed", 3)
   expect_equal(sum(result$Z0), 3)
 
   # Test that spreading infection with fixed Z0 and W gives expected results
-  result <- spread.infection(net, NULL, NULL, "nodes", NULL, FALSE, TRUE, Z0, W)
+  result <- spread.infection(net, NULL, NULL, "nodes", NULL, FALSE, Z0, W)
   expect_equal(result$Z0, Z0)
   expect_equal(result$Z, Z.expect)
   expect_true(all(as.sociomatrix(result$W.net) == W))
