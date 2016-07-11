120909827553fec12a789a9400a69fcc3dcdf96c
Terri Yu <terri@terriyu.info>
2016-07-11 16:36:07 -0400

    Add contact tracing sampling and tests

---
+++ ct_sampling.R
@@ -0,0 +1,174 @@
+# Contact tracing sampling
+
+# Sampling designs are in Chapter 4.6
+
+# Load network package without all the startup messages
+suppressMessages(library(network))
+
+.sample.next <- function(S.prev, S, Z, socio.mtx, design, p.trace.infected, p.trace.uninfected) {
+  # Return next sample in contact tracing process, given sample from previous wave and current sample
+  #
+  # NOTE: This is a private helper function for ct.sample()
+  #
+  # Args:
+  #   S.prev - sample from previous wave as vector
+  #   S - current sample as vector
+  #   Z - infected nodes as vector
+  #   socio.mtx - adjacency matrix corresponding to socio matrix
+  #   design - contact tracing design to use: "infected_only", "infected_and_edge_units", "contacts_of_edge_units", "full_contact_components"
+  #   p.trace.infected - Bernoulli parameter for tracing infected contact
+  #   p.trace.uninfected - Bernoulli parameter for tracing uninfected contact
+  #
+  # Returns:
+  #   S.next - next sample as vector
+
+  # Links can be followed with complete accuracy
+  transmission.mtx <- socio.mtx
+  # Links cannot be followed with complete accuracy
+  if ((! is.null(p.trace.infected)) & (! is.null(p.trace.uninfected))) {
+    num.nodes <- length(Z)
+    num.infect <- sum(Z)
+    # Degrade links to uninfected nodes with Bernoulli parameter p.trace.uninfected
+    transmission.mtx[ , Z == 0] <- transmission.mtx[ , Z == 0] * matrix(rbinom((num.nodes - num.infect) * num.nodes, 1, p.trace.uninfected), nrow = num.nodes, ncol = num.nodes - num.infect)
+    # Degrade links to infected nodes with Bernoulli parameter p.trace.infected
+    transmission.mtx[ , Z == 1] <- transmission.mtx[ , Z == 1] * matrix(rbinom(num.infect * num.nodes, 1, p.trace.infected), nrow = num.nodes, ncol = num.infect)
+  }
+
+  # Compute next sample
+  if (design == "infected_only") {
+    # Only sample infected nodes
+    S.next <- (S.prev %*% transmission.mtx) * Z * (1 - S)
+    S.next <- as.vector(S.next) 
+    S.next[S.next > 0] <- 1
+  } else if ((design == "infected_and_edge_units") | (design == "contacts_of_edge_units")) {
+    # Sample both infected and uninfected nodes but only trace infected nodes
+    S.next <- ((Z * S.prev) %*% transmission.mtx) * (1 - S)
+    S.next <- as.vector(S.next) 
+    S.next[S.next > 0] <- 1
+  } else if (design == "full_contact_components") {
+    # Sample and trace both infected and uninfected nodes
+    S.next <- (S.prev %*% transmission.mtx) * (1 - S)
+    S.next <- as.vector(S.next) 
+    S.next[S.next > 0] <- 1
+  } else {
+    stop("Invalid value specified for design")
+  }
+
+  # Return next sample
+  return(S.next)
+}
+
+ct.sample <- function(disease.net, sigma, design, size.S0 = NULL, num.waves = NULL, p.trace.infected = NULL, p.trace.uninfected = NULL, verbose = FALSE, S0.fixed = NULL) {
+  # Perform contact tracing sample of an infected network
+  #
+  # Args:
+  #   disease.net - network with infection attributes
+  #   sigma - Bernoulli parameter for initial sampling process 
+  #   design - contact tracing design to use: "infected_only", "infected_and_edge_units", "contacts_of_edge_units", "full_contact_components"
+  #   size.S0 - [optional] fix size of initial sample to avoid returning empty sample 
+  #   num.waves - [optional] number of waves to use in sampling (NULL if you keep sampling until no new nodes are sampled)
+  #   p.trace.infected - [optional] Bernoulli parameter for tracing infected contact
+  #   p.trace.uninfected - [optional] Bernoulli parameter for tracing uninfected contact
+  #   verbose - [optional] flag to print variables during sampling process (for debugging) 
+  #   S0.fixed - [optional] nodes in initial sample (only use for debugging/testing)
+  #
+  # Returns:
+  #   S0 - initial nodes sampled as vector
+  #   S - full contact tracing sample as vector
+
+  # Error checking for sigma, p.trace.infected, and p.trace.uninfected
+  if (! is.null(sigma)) {
+    if ((sigma < 0) | (sigma > 1)) {
+      stop("Bernoulli process parameter sigma must be between 0 and 1")
+    }
+  }
+  if (! is.null(p.trace.infected)) {
+    if ((p.trace.infected < 0) | (p.trace.infected > 1)) {
+      stop("p.trace.infected must be between 0 and 1")
+    }
+  }
+  if (! is.null(p.trace.uninfected)) {
+    if ((p.trace.uninfected < 0) | (p.trace.uninfected > 1)) {
+      stop("p.trace.uninfected must be between 0 and 1")
+    }
+  }
+  if (xor(is.null(p.trace.infected), is.null(p.trace.uninfected))) {
+    stop("Both p.trace.infected and p.trace.uninfected need to be specified")
+  }
+
+  # Number of nodes in network
+  num.nodes <- network.size(disease.net)
+  # Extract infected nodes in network 
+  Z <- get.vertex.attribute(disease.net, "infected")
+  # Extract adjacency matrix for disease network
+  socio.mtx <- as.sociomatrix(disease.net)
+ 
+  # Initial sample
+  if (! is.null(S0.fixed)) {
+    # Initial sample is completely specified ahead of time
+    S0 <- S0.fixed
+  } else if (! is.null(size.S0)) {
+    if ((size.S0 < 1) | (size.S0 > num.nodes)) {
+      stop("size.S0 must be between 1 and number of nodes")
+    }
+    # Fixed number of nodes in initial sample
+    S0 <- rep(0, num.nodes)
+    S0[sample(1:num.nodes, size.S0)] <- 1
+  } else {
+    # Homogenous Bernoulli sample with parameter sigma
+    S0 <- rbinom(num.nodes, 1, sigma)
+  }
+
+  # For "infected_only" design, only keep infected nodes from initial sample
+  if (design == "infected_only") {
+    # Only keep infected nodes
+    S0 <- S0 * Z
+  }
+
+  # Current sample
+  S <- S0
+  # Previous wave of samples 
+  S.prev <- S0 
+
+  if (verbose) {
+    cat("S0:\n")
+    print(S0)
+  }
+
+  if (is.null(num.waves)) {
+    # Keep sampling until no new nodes sampled
+    while (sum(S.prev) > 0) {
+      S.next <- .sample.next(S.prev, S, Z, socio.mtx, design, p.trace.infected, p.trace.uninfected)
+
+      if (verbose) {
+        cat("S.next:\n")
+        print(S.next)
+      }
+
+      # Update samples
+      S <- S + S.next
+      S.prev <- S.next
+    }
+  } else {
+    if (num.waves < 1) {
+      stop("Number of waves needs to be at least 1")
+    }
+
+    # Sample for num.waves
+    for (i in 1:num.waves) { 
+      S.next <- .sample.next(S.prev, S, Z, socio.mtx, design, p.trace.infected, p.trace.uninfected)
+
+      if (verbose) {
+        cat("S.next:\n")
+        print(S.next)
+      }
+
+      # Update samples
+      S <- S + S.next
+      S.prev <- S.next
+    }
+  }
+
+  return(list(S0 = S0, S = S))
+}
+
---
+++ tests/testthat/test_ct_sampling.R
@@ -0,0 +1,143 @@
+context("Test ct_sampling.R")
+
+source('../../ct_sampling.R')
+source('../../simulate_network.R')
+
+###################################################
+
+test_that("Test .sample.next", {
+  set.seed(137)
+  # Infected nodes
+  Z <- c(1, 1, 0, 0, 1, 0)
+  # Sociomatrix Y.ij
+  socio.mtx <- matrix(c(0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0), nrow = 6, ncol = 6)
+
+  # Test error checking
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(0, 0, 1, 1, 1, 0) 
+  expect_error(.sample.next(S.prev, S, Z, socio.mtx, "invalid_design", NULL, NULL), regexp = "Invalid value specified for design")
+
+  # Test each of the 4 sampling designs
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(1, 0, 1, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "infected_only", NULL, NULL), c(0, 1, 0, 0, 0, 0))
+
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(1, 0, 1, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "infected_and_edge_units", NULL, NULL), c(0, 0, 0, 0, 0, 0))
+
+  S.prev <- c(1, 0, 0, 0, 0, 0)
+  S <- c(1, 0, 0, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "infected_and_edge_units", NULL, NULL), c(0, 0, 1, 0, 0, 0))
+
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(1, 0, 1, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "contacts_of_edge_units", NULL, NULL), c(0, 0, 0, 0, 0, 0))
+
+  S.prev <- c(1, 0, 0, 0, 0, 0)
+  S <- c(1, 0, 0, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "contacts_of_edge_units", NULL, NULL), c(0, 0, 1, 0, 0, 0))
+
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(1, 0, 1, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "full_contact_components", NULL, NULL), c(0, 1, 0, 1, 0, 0))
+
+  # Test p.trace.infected and p.trace.uninfected
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(1, 0, 1, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "full_contact_components", 1, 1), c(0, 1, 0, 1, 0, 0))
+
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(1, 0, 1, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "full_contact_components", 0, 0), c(0, 0, 0, 0, 0, 0))
+
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(1, 0, 1, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "full_contact_components", 1, 0), c(0, 1, 0, 0, 0, 0))
+
+  S.prev <- c(0, 0, 1, 0, 0, 0)
+  S <- c(1, 0, 1, 0, 0, 1) 
+  expect_equal(.sample.next(S.prev, S, Z, socio.mtx, "full_contact_components", 0, 1), c(0, 0, 0, 1, 0, 0))
+})
+
+test_that("Test ct.sample", {
+  set.seed(137) # Sociomatrix
+  net <- network(matrix(c(0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0), nrow = 6, ncol = 6), directed = FALSE)
+  # Transmission matrix
+  W <- matrix(c(0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0), nrow = 6, ncol = 6)
+  # Initial infected nodes
+  Z0 <- c(1,0,0,0,0,0)
+  # Compute infected network
+  result <- spread.infection(net, NULL, NULL, "nodes", NULL, FALSE, Z0, W)
+  disease.net <- result$disease.net
+  Z <- get.vertex.attribute(disease.net, "infected")
+
+  # Test error checking
+  expect_error(ct.sample(disease.net, -1, "infected_only"), regexp = "Bernoulli process parameter sigma must be between 0 and 1")
+  expect_error(ct.sample(disease.net, 99, "infected_only"), regexp = "Bernoulli process parameter sigma must be between 0 and 1")
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", NULL, NULL, -2), regexp = "p.trace.infected must be between 0 and 1")
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", NULL, NULL, 256), regexp = "p.trace.infected must be between 0 and 1")
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", NULL, NULL, 0.5, 137), regexp = "p.trace.uninfected must be between 0 and 1")
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", NULL, NULL, 0.5, -5), regexp = "p.trace.uninfected must be between 0 and 1")
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", NULL, NULL, 0.5, NULL), regexp = "Both p.trace.infected and p.trace.uninfected need to be specified")
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", NULL, NULL, NULL, 0.5), regexp = "Both p.trace.infected and p.trace.uninfected need to be specified")
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", 0), regexp = "size.S0 must be between 1 and number of nodes") 
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", 10), regexp = "size.S0 must be between 1 and number of nodes") 
+  expect_error(ct.sample(disease.net, 0.2, "infected_only", NULL, 0), regexp = "Number of waves needs to be at least 1") 
+  expect_error(ct.sample(disease.net, 0.2, "invalid_design"), regexp = "Invalid value specified for design") 
+
+  # Test size.S0
+  result <- ct.sample(disease.net, 0.2, "contacts_of_edge_units", 2) 
+  expect_equal(sum(result$S0), 2)
+
+  # Test num.waves
+  S0 <- c(0, 0, 0, 0, 0, 1)
+  result <- ct.sample(disease.net, NULL, "full_contact_components", NULL, 100, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S, c(0, 0, 0, 0, 0, 1))
+
+  S0 <- c(1, 0, 0, 0, 0, 1)
+  result <- ct.sample(disease.net, NULL, "full_contact_components", NULL, 1, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S, c(1, 0, 1, 0, 0, 1))
+
+  # Test "infected_only" design
+  S0 <- c(1, 1, 1, 1, 1, 1)
+  result <- ct.sample(disease.net, NULL, "infected_only", NULL, NULL, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S0, Z)
+
+  S0 <- c(0, 0, 0, 1, 0, 0)
+  result <- ct.sample(disease.net, NULL, "infected_only", NULL, NULL, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S, c(1, 1, 1, 1, 0, 0))
+
+  # Test "infected_and_edge_units" design
+  S0 <- c(0, 0, 0, 0, 0, 1)
+  result <- ct.sample(disease.net, NULL, "infected_and_edge_units", NULL, NULL, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S0, S0)
+  expect_equal(result$S, S0)
+
+  S0 <- c(0, 0, 0, 1, 0, 0)
+  result <- ct.sample(disease.net, NULL, "infected_and_edge_units", NULL, NULL, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S0, S0)
+  expect_equal(result$S, c(1, 1, 1, 1, 1, 0))
+
+  # Test "contacts_of_edge_units" design
+  S0 <- c(0, 0, 0, 0, 0, 1)
+  result <- ct.sample(disease.net, NULL, "contacts_of_edge_units", NULL, NULL, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S0, S0)
+  expect_equal(result$S, S0)
+
+  S0 <- c(0, 0, 0, 1, 0, 0)
+  result <- ct.sample(disease.net, NULL, "contacts_of_edge_units", NULL, NULL, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S0, S0)
+  expect_equal(result$S, c(1, 1, 1, 1, 1, 0))
+
+  # Test "full_contact_components" design
+  S0 <- c(0, 0, 0, 0, 0, 1)
+  result <- ct.sample(disease.net, NULL, "full_contact_components", NULL, NULL, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S0, S0)
+  expect_equal(result$S, S0)
+
+  S0 <- c(0, 0, 0, 0, 1, 0)
+  result <- ct.sample(disease.net, NULL, "full_contact_components", NULL, NULL, NULL, NULL, FALSE, S0) 
+  expect_equal(result$S0, S0)
+  expect_equal(result$S, c(1, 1, 1, 1, 1, 0))
+}) 
